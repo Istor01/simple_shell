57932a83e7364c40267785f2ac1325af3a1555d6
Hlogie01 <mllehlogonolo@gmail.com>
2023-09-20 19:32:33 -0700

    simple shell

--- string.c
+++ string.c
@@ -1,75 +1,75 @@
 #include "shell.h"
 
 /**
- * _strlen - To measure the length of the string
- * @s: The string whose length is to be calculated
+ * _strlen - Returns the length of a string
+ * @s: The string whose length has to be checked
  *
- * Return: The length of the string as an integer
+ * Return: Integer length of the string
  */
-int _strlen(char s)
+int _strlen(char *s)
 {
-	int l = 0;
+	int i = 0;
 
 	if (!s)
 		return (0);
 
-	while (s++)
+	while (*s++)
 		i++;
 	return (i);
 }
 
 /**
- * _strcmp - To compare the two strings lexicographically
- * @s1: The first string
- * @s2: The second string
+ * _strcmp - Performs lexicogarphic comparison of the two strangs
+ * @s1: The first strang
+ * @s2: The second strang
  *
- * Return: A negative value if s1 < s2, positive if s1 > s2, 0 if s1 == s2
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-int _strcmp(char s1, char s2)
+int _strcmp(char *s1, char *s2)
 {
-	while (s1 && s2)
+	while (*s1 && *s2)
 	{
-		if (s1 != s2)
-			return (s1 - s2);
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 		s1++;
 		s2++;
 	}
-	if (s1 == s2)
+	if (*s1 == *s2)
 		return (0);
 	else
-		return  (s1 < s2 ? -1 : 1);
+		return (*s1 < *s2 ? -1 : 1);
 }
 
 /**
- * starts_with - To ensure if a string starts with a specified substring
- * @haystack: The string that is to be searched
- * @needle: The string to find at beginning of haystack
+ * starts_with - Checks if the needle starts with haystack
+ * @haystack: String to search
+ * @needle: The substring to find
  *
- * Return: Address of the nect character in haystack, or NULL if is not found
+ * Return: address of next char of haystack or NULL
  */
-char starts_with(const char haystack, const char needle)
+char *starts_with(const char *haystack, const char *needle)
 {
-	while (needle)
-		if (needle++ != haystack++)
+	while (*needle)
+		if (*needle++ != *haystack++)
 			return (NULL);
 	return ((char *)haystack);
 }
 
 /**
- * _strcat - Attach the two strings together
- * @dest: The destination buffer which source will append
- * @src: The source buffer to append to destination
+ * _strcat - Concatenates the two strings
+ * @dest: The destination buffer
+ * @src: The source buffer
  *
- * Return: Pointer to the destination buffer.
+ * Return: Pointer to the destination buffer
  */
-char _strcat(char dest, char src)
+char *_strcat(char *dest, char *src)
 {
-	char ret = dest;
+	char *ret = dest;
 
-	while (dest)
+	while (*dest)
 		dest++;
-	while (src)
-		dest++ = src++;
-	dest = src;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
 	return (ret);
 }
