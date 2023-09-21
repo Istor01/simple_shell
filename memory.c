b412db39cf64917885e159580a5ffe74a8dcdf98
Hlogie01 <mllehlogonolo@gmail.com>
2023-09-21 14:29:58 -0700

    simple shell

--- memory.c
+++ memory.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
- * bfree - To free a pointer and to NULL the address
- * @ptr: The address of the pointer to be free
+ * bfree - Frees a pointer and NULL the address
+ * @ptr: Address of pointer to free
  *
- * Return: 1 if it is freed, 0 if it is not freed
+ * Return: 1 if freed, otherwise 0.
  */
-int bfree(void ptr)
+int bfree(void **ptr)
 {
-	if (ptr && ptr)
+	if (ptr && *ptr)
 	{
-		free(ptr);
-		ptr = NULL;
+		free(*ptr);
+		*ptr = NULL;
 		return (1);
 	}
 	return (0);
