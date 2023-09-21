71340cbe28c69fe4d58ec7ba328ff86ae954042b
Hlogie01 <mllehlogonolo@gmail.com>
2023-09-21 14:25:05 -0700

    simple shell

--- getLine.c
+++ getLine.c
@@ -18,11 +18,8 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 		free(*buf);
 		*buf = NULL;
 		signal(SIGINT, sigintHandler);
-#
 		r = getline(buf, &len_p, stdin);
-#else
 		r = _getline(info, buf, &len_p);
-#endif
 		if (r > 0)
 		{
 			if ((*buf)[r - 1] == '\n')
